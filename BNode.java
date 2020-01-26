14507380ba4af28f29e398dfac7ee0c88f45c21e
Vinayaka_Raju_S <30818966+vinayakaraju46@users.noreply.github.com>
2020-01-26 12:09:35 +0530

    Add files via upload

---
+++ BNode.java
@@ -0,0 +1,13 @@
+public class BNode
+{
+    public int data;
+    public BNode left;
+    public BNode right;
+
+    public BNode(int val)
+    {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    } 
+}
